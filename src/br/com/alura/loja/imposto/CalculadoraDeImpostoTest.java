e4957065e6bcade40659e6720f6b9eb511452d8c
André Luiz Giannattasio Marques Dias <andregmdias@gmail.com>
2022-07-26 13:52:37 -0300

    refactor: refatora calculadora de impostos aplicando dp strategy

--- src/br/com/alura/loja/imposto/CalculadoraDeImpostoTest.java
+++ src/br/com/alura/loja/imposto/CalculadoraDeImpostoTest.java
@@ -4,28 +4,32 @@ import br.com.alura.loja.orcamento.Orcamento;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.math.BigDecimal;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CalculadoraDeImpostoTest {
+
+    private CalculadoraDeImposto calculadoraDeImposto;
+
+    @BeforeAll
+    void setup() {
+        calculadoraDeImposto = new CalculadoraDeImposto();
+    }
 
     @Test
     public void calculaValorImpostoQuandoRecebeICMSComoTipoImposto() {
-        CalculadoraDeImposto calculadoraDeImposto = new CalculadoraDeImposto();
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
-        TipoImposto tipoImposto = TipoImposto.ICMS;
-        BigDecimal impostoCalculado = calculadoraDeImposto.calcular(orcamento, tipoImposto);
+        BigDecimal impostoCalculado = calculadoraDeImposto.calcular(orcamento, new ICMS());
         Assertions.assertEquals(new BigDecimal("10.0"), impostoCalculado);
     }
 
     @Test
     public void calculaValorImpostoQuandoRecebeISSComoTipoImposto() {
-        CalculadoraDeImposto calculadoraDeImposto = new CalculadoraDeImposto();
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
-        TipoImposto tipoImposto = TipoImposto.ISS;
-        BigDecimal impostoCalculado = calculadoraDeImposto.calcular(orcamento, tipoImposto);
+        BigDecimal impostoCalculado = calculadoraDeImposto.calcular(orcamento, new ISS());
         Assertions.assertEquals(new BigDecimal("6.00"), impostoCalculado);
     }
-
 
 }
